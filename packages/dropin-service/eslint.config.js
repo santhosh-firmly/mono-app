0245e664d7eac6923e363e2cb6aaf1fbc27d9f1b
Washington Junior <washington@firmly.ai>
2025-04-10 19:35:14 -0300

    use .gitignore from root on eslint

--- packages/dropin-service/eslint.config.js
+++ packages/dropin-service/eslint.config.js
@@ -1,11 +1,7 @@
-import { fileURLToPath } from 'node:url';
 import createConfig from '@mono-app/eslint-config';
 import svelteConfig from './svelte.config.js';
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
-
 export default createConfig({
-	gitignorePath,
 	svelteConfig,
 	// Temporary because v4 have a lot of Svelte lint errors
 	ignores: ['**/src/lib-v4/**']
