db6c1d6a4c3e503d9dd93bbd8fb0978250e23a9d
Washington Junior <washington@firmly.ai>
2025-04-16 15:18:46 -0300

    update eslint

--- packages/dash/eslint.config.js
+++ packages/dash/eslint.config.js
@@ -1,23 +1,6 @@
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import prettier from 'eslint-config-prettier';
-import globals from 'globals';
+import createConfig from '@firmly/eslint-config';
+import svelteConfig from './svelte.config.js';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-	js.configs.recommended,
-	...svelte.configs['flat/recommended'],
-	prettier,
-	...svelte.configs['flat/prettier'],
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		}
-	},
-	{
-		ignores: ['build/', '.svelte-kit/', 'dist/']
-	}
-];
+export default createConfig({
+	svelteConfig
+});
