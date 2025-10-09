c4df6f7f5344d7282f10bb2500dc249a2b3e5de4
Washington Junior <washington@firmly.ai>
2025-10-09 20:25:49 -0300

    feat: create a simple error component

---
+++ packages/dropin-service/src/lib-v4/components/v4/simple-error.stories.svelte
@@ -0,0 +1,39 @@
+<script>
+	// @ts-nocheck
+	import SimpleError from './simple-error.svelte';
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+	import './theme.css';
+	import { isPrimaryDark } from './theme-context';
+
+	function setDarkTheme() {
+		isPrimaryDark.set(true);
+	}
+
+	function setLightTheme() {
+		isPrimaryDark.set(false);
+	}
+</script>
+
+<Meta
+	title="Checkout V4/Common/Simple Error"
+	component={SimpleError}
+	tags={['autodocs']}
+	parameters={{
+		layout: 'fullscreen'
+	}}
+/>
+
+<Story
+	name="Default Error"
+	args={{
+		errorMessage: 'We encountered an issue loading your cart. Please try again.',
+		pdpUrl: 'https://example.com/product'
+	}}
+>
+	<div class="theme-provider" use:setLightTheme>
+		<SimpleError
+			errorMessage="We encountered an issue loading your cart. Please try again."
+			pdpUrl="https://example.com/product"
+		/>
+	</div>
+</Story>
---
+++ packages/dropin-service/src/lib-v4/components/v4/simple-error.svelte
@@ -0,0 +1,15 @@
+<script>
+	// @ts-nocheck
+
+	let { errorMessage, onBack } = $props();
+</script>
+
+<div class="bg-fy-primary flex h-full w-full flex-col items-center justify-center gap-8 px-6">
+	<div class="flex max-w-md flex-col items-center justify-center gap-6">
+		<span class="text-fy-on-primary text-center text-4xl font-medium"> Oops! </span>
+		<span class="text-fy-on-primary-subtle text-center text-base font-normal">
+			{errorMessage}
+		</span>
+		<button class="text-sm underline" onclick={onBack}> Back </button>
+	</div>
+</div>
