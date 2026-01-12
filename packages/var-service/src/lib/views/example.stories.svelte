77c993b3574c968cb93a6d8a4d5f0ec0db07151c
Washington Junior <washington@firmly.ai>
2026-01-12 15:11:26 -0300

    chore: organize the storybook

---
+++ packages/var-service/src/lib/views/example.stories.svelte
@@ -0,0 +1,27 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Example from './example.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Views/Example',
+		component: Example,
+		parameters: {
+			layout: 'padded'
+		},
+		argTypes: {
+			recording: { control: 'boolean' }
+		}
+	});
+</script>
+
+<Story name="Not Recording" args={{ recording: false, onToggleRecording: () => {} }}>
+	{#snippet template(args)}
+		<Example {...args} />
+	{/snippet}
+</Story>
+
+<Story name="Recording" args={{ recording: true, onToggleRecording: () => {} }}>
+	{#snippet template(args)}
+		<Example {...args} />
+	{/snippet}
+</Story>
